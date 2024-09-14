38c387aa91bdccc5bfdb9a09e27ad2af5f5080ed
Giuseppe86-lab <sinatragi@gmail.com>
2024-09-14 16:11:58 +0200

    Inizializzazione classi docente e classe, ancora da completare

--- 3-TeachersTimetable/classes_sin.py
+++ 3-TeachersTimetable/classes_sin.py
@@ -1,17 +1,19 @@
 import random
+import csv
 
 class Docente:
-    def __init__(self, nome, materie, max_ore, disponibilita):
+    def __init__(self, cognome, nome, materie, max_ore, classi):
+        self.cognome = cognome
         self.nome = nome
         self.materie = materie
         self.max_ore = max_ore
-        self.disponibilita = disponibilita
+        self.classi = classi
         self.ore_associate = 0
 
 class Classe:
-    def __init__(self, nome, materie_assegnate):
+    def __init__(self, nome, docenti_assegnati):
         self.nome = nome
-        self.materie_assegnate = materie_assegnate
+        self.docenti_assegnati = docenti_assegnati
         self.orario = {}
 
 class Orario:
@@ -50,3 +52,11 @@ class Orario:
             classe, slot = chiave
             materia, docente = valore
             print(f"Classe {classe} - Slot {slot}: {materia} con {docente}")
+
+def get_teachers(filepath='teachers.csv'):
+    """ Read a text file and return the list of
+    all the teachers.
+    """
+    with open(filepath, 'r') as file_local:
+        all_teachers = list(csv.reader(file_local))
+    return all_teachers
